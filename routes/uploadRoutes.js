d266b320c4dd92a15f2d611ebac2cbbdf6d49d25
aditya <adityaparuleker.cr@gmail.com>
2025-08-20 15:56:02 +0530

    Upload route

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,8 +1,9 @@
-const express = require('express');
-const uploadController = require('../controllers/uploadController');
-
+const express = require("express");
+const uploadController = require("../controllers/uploadController");
+const multer = require("multer");
 const router = express.Router();
-
-router.post('/generate-presigned-url', uploadController.generatePresignedUrl);
+const upload = multer({ storage: multer.memoryStorage() });
+router.post("/", upload.array("images", 10), uploadController.uploadImages);
+router.post("/generate-presigned-url", uploadController.generatePresignedUrl);
 
 module.exports = router;
