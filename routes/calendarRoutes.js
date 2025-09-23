68b0b51a0d12a1619b2e6225259e76eb5d6652cb
aditya <adityaparuleker.cr@gmail.com>
2025-09-23 17:28:18 +0530

    formatting change

--- routes/calendarRoutes.js
+++ routes/calendarRoutes.js
@@ -1,18 +1,21 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const calendarController = require('../controllers/calendarController');
-const shareController = require('../controllers/shareController');
+const calendarController = require("../controllers/calendarController");
+const shareController = require("../controllers/shareController");
 
 // Get the availability for a specific experience
-router.get('/:experienceId', calendarController.getAvailability);
+router.get("/:experienceId", calendarController.getAvailability);
 
 // Block or unblock a specific date for a given experience
-router.put('/:experienceId/block', calendarController.blockDate);
+router.put("/:experienceId/block", calendarController.blockDate);
 
 // Mark a reservation date as unavailable
-router.put('/:experienceId/unavailable', calendarController.markDateUnavailable);
+router.put(
+  "/:experienceId/unavailable",
+  calendarController.markDateUnavailable
+);
 
 // Share experience availability or booking info with another user
-router.post('/share', shareController.createShare);
+router.post("/share", shareController.createShare);
 
 module.exports = router;
