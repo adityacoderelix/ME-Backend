99a1017c8fc18f5a48955405974eda1032adafdb
aditya <adityaparuleker.cr@gmail.com>
2025-10-02 17:26:34 +0530

    unused

--- services/cronRunner.js
+++ services/cronRunner.js
@@ -1,5 +1,18 @@
-// cronRunner.js
-const cron = require("node-cron");
-const calendarsyncController = require("../controllers/calendarsyncController");
+// // cronRunner.js
 
-// run immediately on startup, then every 5 minutes
+// const calendarsyncController = require("../controllers/calendarsyncController");
+
+// // calendar-cron.js
+// const cron = require("node-cron");
+// const syncCalendars = require("../controllers/calendarsyncController/syncCalendars"); // adjust path
+
+// function startCalendarCron() {
+//   cron.schedule("*/5 * * * *", async () => {
+//     console.log("Running calendar sync...");
+//     await syncCalendars();
+//   });
+// }
+
+// module.exports = startCalendarCron;
+
+// // run immediately on startup, then every 5 minutes
