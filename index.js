fdf6a07a059dd6d1658b22f59640597ffc48fe49
aditya <adityaparuleker.cr@gmail.com>
2025-11-20 10:20:37 +0530

    Removed double webroute in index

--- index.js
+++ index.js
@@ -54,7 +54,7 @@ app.use((req, res, next) => {
   next();
 });
 const webhookRoutes = require("./routes/webhookRoutes");
-const webhookRoutes = require("./routes/webhookRoutes");
+
 app.use("/api/v1/paymentforpayout", webhookRoutes);
 
 app.use(express.json({ limit: "50mb" }));
