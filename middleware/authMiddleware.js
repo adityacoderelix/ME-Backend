01a92266eb29f6d0c2bf190bf96bb20acc3e1e20
aditya <adityaparuleker.cr@gmail.com>
2025-11-21 13:10:41 +0530

    Checking if user is banned for authmiddleware

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,7 +1,9 @@
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
 
 const authMiddleware = async (req, res, next) => {
   try {
+    console.log("Enter authmiddleware");
     const authHeader = req.headers["authorization"];
 
     const token = authHeader && authHeader.split(" ")[1];
@@ -16,15 +18,54 @@ const authMiddleware = async (req, res, next) => {
       });
     }
 
-    const user = await new Promise((resolve, reject) => {
+    const decoded = await new Promise((resolve, reject) => {
       jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
         if (err) reject(err);
         else resolve(decoded);
       });
     });
-
-    req.user = user;
-    next();
+    console.log("Enter banning 0");
+    if (decoded.admin == 0) {
+      console.log("Enter banning", decoded.admin);
+      const user = await User.findById(decoded.userId);
+      if (!user) {
+        return res.status(404).json({
+          success: false,
+          code: "USER_NOT_FOUND",
+          message: "User does not exist",
+          statusCode: 404,
+          requestType: req.method,
+        });
+      }
+      console.log("Enter banning2");
+      // 🚫 BLOCK BANNED USERS IMMEDIATELY
+      if (user.status?.banned === true) {
+        return res.status(403).json({
+          success: false,
+          code: "USER_BANNED",
+          message: "Your account has been banned",
+          statusCode: 403,
+          requestType: req.method,
+        });
+      }
+      console.log("Enter banning3");
+      // 🚫 FORCE LOGOUT IF tokenVersion DOESN’T MATCH
+      if (decoded.tokenVersion !== user.tokenVersion) {
+        return res.status(401).json({
+          success: false,
+          code: "TOKEN_INVALIDATED",
+          message: "Session expired. Please log in again",
+          statusCode: 401,
+          requestType: req.method,
+        });
+      }
+      console.log("Enter banning4");
+      req.user = user;
+      next();
+    } else {
+      req.user = decoded;
+      next();
+    }
   } catch (error) {
     console.error("Auth Middleware Error:", error);
     if (error.name === "TokenExpiredError") {
