d662ab0af0c3b4314def3fcdb8449d116629adfe
aditya <adityaparuleker.cr@gmail.com>
2025-11-21 18:39:17 +0530

    aijdis

--- models/BankDetail.js
+++ models/BankDetail.js
@@ -6,7 +6,6 @@ const bankDetailSchema = new mongoose.Schema(
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
       required: true,
-      unique: true,
     },
     contactId: {
       type: String,
--- models/HostBankAccount.js
+++ models/HostBankAccount.js
@@ -1,35 +1,41 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const HostBankAccountSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true
+const HostBankAccountSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      // unique: true
+    },
+    accountNumber: {
+      type: String,
+      required: true,
+      minlength: 9,
+      maxlength: 18,
+    },
+    ifscCode: {
+      type: String,
+      required: true,
+      match: /^[A-Z]{4}0[A-Z0-9]{6}$/,
+    },
+    accountHolderName: {
+      type: String,
+      required: true,
+      minlength: 2,
+    },
+    bankName: {
+      type: String,
+      required: true,
+      minlength: 2,
+    },
   },
-  accountNumber: {
-    type: String,
-    required: true,
-    minlength: 9,
-    maxlength: 18
-  },
-  ifscCode: {
-    type: String,
-    required: true,
-    match: /^[A-Z]{4}0[A-Z0-9]{6}$/
-  },
-  accountHolderName: {
-    type: String,
-    required: true,
-    minlength: 2
-  },
-  bankName: {
-    type: String,
-    required: true,
-    minlength: 2
-  }
-}, { timestamps: true })
+  { timestamps: true }
+);
 
-const HostBankAccount = mongoose.model("HostBankAccount", HostBankAccountSchema)
+const HostBankAccount = mongoose.model(
+  "HostBankAccount",
+  HostBankAccountSchema
+);
 
-module.exports = HostBankAccount
+module.exports = HostBankAccount;
